a4c38951c68dadcf904cc6805eed5472f0ce8c6b
Niccolo Delfino <delfino.nicco@libero.it>
2021-01-15 16:14:14 +0100

    part 3 ok! All done

--- js/script.js
+++ js/script.js
@@ -13,6 +13,32 @@ for (var key in studente) {
 
 // - Creare un array di oggetti di studenti. Ciclare su tutti gli studenti e stampare per ognuno nome e cognome
 
+// var studenti = [
+//   {
+//     nome : 'Niccolo',
+//     cognome : 'Delfino',
+//     eta : 30
+//   },
+//   {
+//     nome : 'Miku',
+//     cognome : 'Hatsune',
+//     eta : 25
+//   },
+//   {
+//     nome : 'Bulma',
+//     cognome : 'Brief',
+//     eta : 35
+//   },
+// ];
+//
+// for (var i = 0; i < studenti.length; i++) {
+//   console.log(studenti[i].nome);
+//   console.log(studenti[i].cognome);
+// }
+
+// - Dare la possibilità all’utente attraverso 3 prompt di aggiungere un nuovo oggetto studente inserendo nell’ordine: nome, cognome e età.
+
+
 var studenti = [
   {
     nome : 'Niccolo',
@@ -31,9 +57,15 @@ var studenti = [
   },
 ];
 
+var studenteAggiunto = {
+  nome : prompt('Inserisci qui il tuo nome'),
+  cognome : prompt('Inserisci qui il tuo cognome'),
+  eta : prompt('Inserisci qui la tua eta')
+}
+
+studenti.push(studenteAggiunto);
+
 for (var i = 0; i < studenti.length; i++) {
   console.log(studenti[i].nome);
   console.log(studenti[i].cognome);
 }
-
-// - Dare la possibilità all’utente attraverso 3 prompt di aggiungere un nuovo oggetto studente inserendo nell’ordine: nome, cognome e età.
