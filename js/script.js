36e80816aef3d35e4baaa07ecf4a49b346e76d24
Niccolo Delfino <delfino.nicco@libero.it>
2021-01-15 15:52:01 +0100

    Part 1 ok

--- js/script.js
+++ js/script.js
@@ -1,4 +1,15 @@
 // - Creare un oggetto che descriva uno studente con le seguenti proprietà: nome, cognome e età.
 // Stampare a schermo attraverso il for in tutte le proprietà.
+
+var studente ={
+  nome : 'Niccolo',
+  cognome : 'Delfino',
+  eta : 30
+};
+
+for (var key in studente) {
+  console.log(key + ': ' + studente[key]);
+}
+
 // - Creare un array di oggetti di studenti. Ciclare su tutti gli studenti e stampare per ognuno nome e cognome
 // - Dare la possibilità all’utente attraverso 3 prompt di aggiungere un nuovo oggetto studente inserendo nell’ordine: nome, cognome e età.
